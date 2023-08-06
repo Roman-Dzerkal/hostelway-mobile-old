41dec42e71becbd2a7608c6b12f5fecbadfa81fa
mmyr <r.dzerkal@pinta.pro>
2023-08-06 14:28:18 +0300

    Added login page skeleton

--- lib/pages/auth/login/login_view.dart
+++ lib/pages/auth/login/login_view.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:hostelway/helpers/typography.dart';
 import 'package:hostelway/pages/auth/login/login_vm.dart';
 import 'package:stacked/stacked.dart';
 
@@ -7,10 +9,76 @@ class LoginView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // Don't remove this object. It needed to adaptive layout
+    final Size screenSize = MediaQuery.of(context).size;
     return ViewModelBuilder<LoginViewModel>.reactive(
       viewModelBuilder: () => LoginViewModel(),
       onViewModelReady: (viewModel) {},
-      builder: (context, model, child) => Scaffold(),
+      builder: (context, model, child) {
+        return Scaffold(
+          body: SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('Email', style: lableLarge),
+                      TextField(
+                        controller: model.emailController,
+                        maxLines: 1,
+                        decoration: InputDecoration(
+                            hintText: 'Enter your email',
+                            prefixIcon: const Icon(CupertinoIcons.envelope),
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10))),
+                      ),
+                    ],
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('Password', style: lableLarge),
+                      TextField(
+                        controller: model.passwordController,
+                        maxLines: 1,
+                        obscureText: model.obscurePassword,
+                        obscuringCharacter: '*',
+                        decoration: InputDecoration(
+                            hintText: 'Enter your password',
+                            prefixIcon: const Icon(CupertinoIcons.lock),
+                            suffixIcon: IconButton(
+                              onPressed: model.unhidePassword,
+                              icon: Icon(model.obscurePassword
+                                  ? CupertinoIcons.eye
+                                  : CupertinoIcons.eye_slash),
+                            ),
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10))),
+                      ),
+                    ],
+                  ),
+                  Align(
+                    alignment: Alignment.center,
+                    child: TextButton(
+                      onPressed: () {},
+                      style: ButtonStyle(
+                          backgroundColor: MaterialStateColor.resolveWith(
+                              (states) => Colors.blueAccent)),
+                      child: const Text(
+                        'Login',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        );
+      },
     );
   }
 }
--- lib/pages/auth/login/login_vm.dart
+++ lib/pages/auth/login/login_vm.dart
@@ -1,3 +1,14 @@
+import 'package:flutter/material.dart';
 import 'package:stacked/stacked.dart';
 
-class LoginViewModel extends BaseViewModel {}
+class LoginViewModel extends BaseViewModel {
+  final TextEditingController emailController = TextEditingController(),
+      passwordController = TextEditingController();
+
+  bool obscurePassword = true;
+
+  void unhidePassword() {
+    obscurePassword = !obscurePassword;
+    notifyListeners();
+  }
+}
